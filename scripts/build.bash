022f2b7e9a7c275092f88dd8211f745eacd9b4bc
buebos <98134814+buebos@users.noreply.github.com>
2024-05-02 01:07:26 -0700

    Fix (scripts/build.bash): Added OS check before calling the build command.

--- scripts/build.bash
+++ scripts/build.bash
@@ -1,3 +1,12 @@
 # TODO: This script should be able to build the project directly parsing the compile_commands JSON or something.
+kernel_release="$(uname -r)"
 
-cmd.exe /C "gcc src/util/log/log.c src/util/app.c src/app/app_setup_common_args.c src/app/config/json/read.c src/app/config/app_setup_config.c src/help/help.c src/run/run.c src/build/build.c src/header/header.c src/app_set_command.c main.c -o build/cres"
+if [[ "$kernel_release" == *"Microsoft"* || "$kernel_release" == *"microsoft"* ]]; then
+    is_windows=true
+fi
+
+if [[ $is_windows ]]; then
+    cmd.exe /C "gcc src/util/log/log.c src/util/app.c src/app/app_setup_common_args.c src/app/config/json/read.c src/app/config/app_setup_config.c src/help/help.c src/run/run.c src/build/build.c src/header/header.c src/app_set_command.c main.c -o build/cres"
+else
+    gcc src/util/log/log.c src/util/app.c src/app/app_setup_common_args.c src/app/config/json/read.c src/app/config/app_setup_config.c src/help/help.c src/run/run.c src/build/build.c src/header/header.c src/app_set_command.c main.c -o build/cres
+fi
