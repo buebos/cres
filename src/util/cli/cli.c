8b0e902fb6b0bc63d7833253f83d52c6a7f5ccd9
Gael Herrera <hrac.gael@gmail.com>
2024-05-10 20:44:03 -0700

    Chore: Sync commit.

--- src/util/cli/cli.c
+++ src/util/cli/cli.c
@@ -3,32 +3,28 @@
 const CliCommand Build = {
     .id = "build",
     .aliases = NULL,
+    .run = NULL,
 
     .cmds_len = 0,
     .cmds = NULL,
 
-    .run = NULL,
+    .params_len = 1,
+    .params = &(CliParam){
+        .id = "entry",
+        .type = CLI_STR,
+        .is_list = false,
+        .is_nullable = true,
+        .value_constraints = NULL,
+    },
 
-    .args_len = 3,
-    .args = &(CliArg[]){
-        {
-            .id = "entry",
-            .type = CLI_ARG_POSITIONAL,
-            .params_len = 0,
-            .params = NULL,
-            .positional = &(CliValue){
-                .id = "entry",
-                .type = CLI_PATH_FILE,
-                .is_list = false,
-                .is_nullable = true,
-                .value_constraints = NULL,
-            },
-        },
+    .flags_len = 3,
+    .flags = &(CliFlag[]){
         {
             .id = "variant",
-            .type = CLI_ARG_FLAG,
+            .aliases_len = 1,
+            .aliases = &(char*[]){"var"},
             .params_len = 1,
-            .params = &(CliValue[]){
+            .params = &(CliParam[]){
                 {
                     .id = "variant",
                     .type = CLI_STR,
@@ -40,9 +36,10 @@ const CliCommand Build = {
         },
         {
             .id = "targets",
-            .type = CLI_ARG_FLAG,
+            .aliases_len = 1,
+            .aliases = &(char*[]){"tgs"},
             .params_len = 1,
-            .params = &(CliValue[]){
+            .params = &(CliParam[]){
                 {
                     .id = "targets",
                     .type = CLI_STR,
@@ -60,37 +57,80 @@ const CliCommand Build = {
     },
 };
 
-bool cli_is_flag_delimiter(char* tok) {
+bool cli_tok_is_flag(char* tok) {
     return tok[0] == '-';
 }
+CliParseResult cli_parse_unknown_tok(int i) {
+    return (CliParseResult){
+        .status = CLI_PARSE_UNKNOWN_TOK,
+        .data = {
+            .unknown_token = {
+                .good_until_index = i,
+            },
+        },
+    };
+}
+
+CliFlag* cli_flag_get(CliCommand* cmd, char* tok) {
+    for (int i = 0; i < cmd->flags_len; i++) {
+        CliFlag* flag = &cmd->flags[i];
+
+        if (strcmp(flag->id, tok) == 0) {
+            return flag;
+        }
+
+        for (int j = 0; j < flag->aliases_len; i++) {
+            char* flag_alias = &flag->aliases[i];
+
+            if (strcmp(flag_alias, tok) == 0) {
+                return flag;
+            }
+        }
+    }
+
+    return NULL;
+}
+CliParseResult cli_flag_validate_params(CliFlag* flag) {}
 
 CliParseResult cli_parse(Cli* cli, int argc, char** argv) {
-    CliArg* arg_current = NULL;
+    if (!cli->root) {
+        return (CliParseResult){
+            .status = CLI_PARSE_SUCCESS,
+            .data = {0},
+        };
+    }
+
+    CliFlag* flag_current = NULL;
     CliCommand* cmd_current = cli->root;
 
-    /** For positional arguments assignment */
-    size_t arg_cursor = 0;
+    size_t cmd_params_cursor = 0;
+    size_t flag_params_cursor = 0;
 
     for (int i = 1; i < argc; i++) {
         char* tok = argv[i];
 
-        if (cli_is_flag_delimiter(tok)) {
+        if (cli_tok_is_flag(tok)) {
+            CliFlag* flag_match = cli_flag_get(cmd_current, tok);
+
+            if (!flag_match) {
+                return cli_parse_unknown_tok(i);
+            }
+
+            CliParseResult res = cli_flag_validate_params(flag_current);
+
+            if (res.status != CLI_PARSE_SUCCESS) {
+                return res;
+            }
+
+            flag_current = flag_match;
+
+            continue;
         }
 
+        CliCommand* cmd_match = cli_cmd_get(cmd_current, tok);
+        CliParam* param_match = cli_param_get(cmd_current, tok);
+
         /** The token did not match any cmd or arg in scope */
-        return (CliParseResult){
-            .status = CLI_UNKNOWN_TOK,
-            .data = {
-                .unknown_token = {
-                    .good_until_index = i,
-                },
-            },
-        };
-    }
-
-    arg_current = NULL;
-    cmd_current = cli->root;
-
-    while (cmd_current != NULL) {
+        return cli_parse_unknown_tok(i);
     }
 }
--- src/util/cli/cli.h
+++ src/util/cli/cli.h
@@ -8,20 +8,20 @@
 
 #include "../ds/stack.h"
 
-#define CLI_ARGS(preargs)                                          \
-    (&(CliArg[]) {                                                 \
-        CliArg args[] = preargs;                                   \
-        for (int i = 0; i < sizeof(args) / sizeof(args[0]; i++)) { \
-            CliArg arg = args[i];                                  \
-        }                                                          \
-        args;                                                      \
+#define CLI_ARGS(preargs)                                            \
+    (&(CliFlag[]) {                                                  \
+        CliFlag flags[] = preargs;                                   \
+        for (int i = 0; i < sizeof(flags) / sizeof(flags[0]; i++)) { \
+            CliFlag arg = flags[i];                                  \
+        }                                                            \
+        flags;                                                       \
     })
 
 typedef enum CliParseResStatus {
     CLI_PARSE_SUCCESS,
-    CLI_UNKNOWN_TOK,
-    CLI_INVALID_ARG_TYPE,
-    CLI_MISSING_ARG,
+    CLI_PARSE_UNKNOWN_TOK,
+    CLI_PARSE_INVALID_ARG_TYPE,
+    CLI_PARSE_MISSING_ARG,
 } CliParseResStatus;
 
 typedef struct CliParseUnknownTok {
@@ -37,12 +37,7 @@ typedef struct CliParseResult {
     CliParseResData data;
 } CliParseResult;
 
-typedef enum CliArgType {
-    CLI_ARG_FLAG,
-    CLI_ARG_POSITIONAL,
-} CliArgType;
-
-typedef enum CliValueType {
+typedef enum CliParamType {
     CLI_BOOL,
 
     CLI_INT,
@@ -52,42 +47,45 @@ typedef enum CliValueType {
 
     CLI_PATH_FILE,
     CLI_PATH_DIR,
-} CliValueType;
+} CliParamType;
 
-typedef struct CliValue {
+typedef struct CliParam {
     char* id;
-    CliValueType type;
+    CliParamType type;
 
     bool is_list;
     bool is_nullable;
 
     char** value_constraints;
 
-    void* value;
-} CliValue;
+    void* _value;
+} CliParam;
 
-typedef struct CliArg {
+typedef struct CliFlag {
     char* id;
-    CliArgType type;
-    CliValue* positional;
 
-    CliValue* params;
+    char** aliases;
+    size_t aliases_len;
+
+    CliParam* params;
     size_t params_len;
-} CliArg;
+} CliFlag;
 
 typedef struct CliCommand {
     char* id;
     char** aliases;
+    void* run;
 
     CliCommand* cmds;
     size_t cmds_len;
 
-    CliArg* args;
-    size_t args_len;
+    CliParam* params;
+    size_t params_len;
+
+    CliFlag* flags;
+    size_t flags_len;
 
     CliCommand* _command;
-
-    void* run;
 } CliCommand;
 
 typedef struct Cli {
@@ -97,6 +95,6 @@ typedef struct Cli {
 } Cli;
 
 CliParseResult cli_parse(Cli* cli, int argc, char** argv);
-void* cli_arg_get(CliArg* args, char* arg_id);
+void* cli_arg_get(CliFlag* flags, char* arg_id);
 
 #endif  // !__CLI_H__
